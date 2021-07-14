1085db04ce21fb0d84306b1edadb2e72b4a544c4
Yuval Rosen <yuv.rosen@gmail.com>
2021-07-14 11:58:21 +0300

    first commit

---
+++ examples/test.py
@@ -0,0 +1,7 @@
+def main():
+    num = [0]
+    # num = [0, 1]
+
+    print(num[0])
+
+    return 0
---
+++ file_start.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <math.h>
+
+clock_t __host_start_time__;
+clock_t __host_end_time__;
+
+void start_host_timer(){
+    /* Call to start HOST timer which can be stopped with `stop_host_timer`, returns nothing.
+    */
+    __host_start_time__ = clock();
+}
+
+float stop_host_timer(){
+    /* Call to stop HOST timer started with `start_host_timer`, returns elapsed time in seconds.
+    */
+    __host_start_time__ = clock();
+    return (__host_end_time__ - __host_start_time__) / CLOCKS_PER_SEC;
+}
